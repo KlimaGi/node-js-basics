b60315aa4a5f6fb05ceb7f9eec3a4a109c61506f
Giedre Klimaite <giedre.klima@gmail.com>
2021-10-23 21:36:24 +0300

    importove a module with EventEmitter

--- node-essential/lib/collectAnswers.js
+++ node-essential/lib/collectAnswers.js
@@ -1,4 +1,7 @@
 const readline = require("readline");
+const { EventEmitter } = require("events");
+const { emit } = require("process");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -7,17 +10,22 @@ const rl = readline.createInterface({
 module.exports = (questions, done = (f) => f) => {
   const answers = [];
   const [firstQ] = questions;
+  const emitter = new EventEmitter();
 
   const questionAnswered = (answer) => {
+    emitter.emit("answer", answer);
     answers.push(answer);
     if (answers.length < questions.length) {
       rl.question(questions[answers.length], questionAnswered);
     } else {
+      emitter.emit("complete", answers);
       done(answers);
     }
   };
 
   rl.question(firstQ, questionAnswered);
+
+  return emitter;
 };
 
 // collectAnswers(questions, (answers) => {
